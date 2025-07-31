5d4603778fe68b640d1ff01ca74be0e38c1eadd9
BulatovaTati <kohodatat@gmail.com>
2025-07-31 12:50:15 +0300

    add open modal to RecommendedBooksList

--- src/components/RecommendedBooksList/RecommendedBooksList.tsx
+++ src/components/RecommendedBooksList/RecommendedBooksList.tsx
@@ -1,16 +1,29 @@
 import { FC } from 'react';
 import { Book } from '../../redux/books/books-types';
 import s from './RecommendedBooksList.module.css';
+import { useModalContext } from '../../context/ModalContext';
 
 interface RecommendedBooksListProps {
   results: Book[];
 }
 
 const RecommendedBooksList: FC<RecommendedBooksListProps> = ({ results }) => {
+  const { openModal } = useModalContext();
+
+  const handleClick = (book: Book) => {
+    openModal('bookInfo', {
+      bookId: book._id,
+      title: book.title,
+      author: book.author,
+      totalPages: book.totalPages,
+      imageUrl: book.imageUrl,
+    });
+  };
+
   return (
     <ul className={s.list}>
       {results?.slice(0, 3).map(book => (
-        <li className={s.item} key={book._id}>
+        <li className={s.item} key={book._id} onClick={() => handleClick(book)}>
           <img src={book.imageUrl} alt={book.title} className={s.image} />
           <p className={s.title}>{book.title}</p>
           <p className={s.author}>{book.author}</p>
