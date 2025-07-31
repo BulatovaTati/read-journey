927fcd6aeba6852b4f4cf4b89c1eb07b66b5ed4b
BulatovaTati <kohodatat@gmail.com>
2025-07-31 12:45:00 +0300

    add pagination to library list

--- src/components/MyLibrary/MyLibrary.tsx
+++ src/components/MyLibrary/MyLibrary.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch } from '../../redux/hooks';
 import CreateLibrary from '../CreateLibrary/CreateLibrary';
 import Dashboard from '../Dashboard/Dashboard';
@@ -9,14 +9,29 @@ import RecommendedList from '../RecommendedList/RecommendedList';
 import { useSelector } from 'react-redux';
 import { selectOwnBooks } from '../../redux/books/selectors';
 import CustomSelect from '../CustomSelect/CustomSelect';
+import Pagination from '../Pagination/Pagination';
+import useMedia from '../../hooks/useMedia';
 
 const MyLibrary = () => {
   const dispatch = useAppDispatch();
   const ownLibrary = useSelector(selectOwnBooks);
 
+  const [page, setPage] = useState<number>(1);
+  const { isMobile, isTablet, isDesktop } = useMedia();
+  const limit = isMobile ? 2 : isTablet ? 8 : 10;
+
   useEffect(() => {
     dispatch(fetchOwnBooks());
   }, [dispatch]);
+
+  const totalPages = Math.ceil(ownLibrary.length / limit);
+  const paginatedBooks = ownLibrary.slice((page - 1) * limit, page * limit);
+
+  const handlePageChange = (newPage: number): void => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setPage(newPage);
+    }
+  };
 
   return (
     <>
@@ -29,8 +44,9 @@ const MyLibrary = () => {
         <div className={s.select}>
           <h1 className={s.title}>My library</h1>
           <CustomSelect />
+          <Pagination totalPages={totalPages} handlePageChange={handlePageChange} page={page} />
         </div>
-        <RecommendedList results={ownLibrary} />
+        <RecommendedList results={paginatedBooks} />
       </div>
     </>
   );
